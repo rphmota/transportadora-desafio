d9187cd6c613ca51fa88e3defec2cde4136d3bd7
Raphael Mota <rphmota@gmail.com>
2024-05-10 19:49:20 -0300

    commit inicial

---
+++ src/main/java/com/rphmota/transportadoradesafio/TransportadoraDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.rphmota.transportadoradesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportadoraDesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportadoraDesafioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rphmota/transportadoradesafio/TransportadoraDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rphmota.transportadoradesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportadoraDesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
